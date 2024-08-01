5152da0d10556651a2a6f61693369037f9155bdf
Ilyas Shahmuradov <ilyas.shahmuradov@mail.ru>
2024-08-01 17:51:59 +0400

    Categories Page developed

--- src/Pages/CategoriesPage/CategoriesPage.js
+++ src/Pages/CategoriesPage/CategoriesPage.js
@@ -1,12 +1,15 @@
 import { PopularCategoriesItem } from "../../components/PopularCategoriesItem/PopularCategoriesItem";
+import "./CategoriesPage.scss";
+import CategoryFrame from "../../assets/images/Categories.jpg";
 
 export const CategoriesPage = () => {
   return (
     <>
-      <div className="popular-categories">
-        <div className="popular-categories-top">
-          <div className="popular-categories-top-title">Все Категории</div>
-        </div>
+      <div className="all-categories-frame">
+        <img src={CategoryFrame} alt="frame" />
+        <div className="all-categories-frame-title">Все категории</div>
+      </div>
+      <div className="all-categories">
         <PopularCategoriesItem />
       </div>
     </>
